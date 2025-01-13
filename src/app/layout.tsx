d971d0c77ca866db1a0e5200f98ff2cb951cceb4
x0finch <x0finch0x@gmail.com>
2025-01-13 15:01:38 +0800

    setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Ethereum Calldata Decoder",
+  description: "Decode Ethereum Calldata As Easy As We Go",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@shadcn/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="mx-auto">
+      <Button>Click Me</Button>
+    </div>
+  )
+}
