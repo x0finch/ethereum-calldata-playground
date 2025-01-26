57ad0110571a0cd03a07d7d1e9c08cfc9a5def7f
x0finch <x0finch0x@gmail.com>
2025-01-26 23:20:55 +0800

    Remove useless dependences

--- src/components/rich-actions-text-field/rich-actions-text-field.tsx
+++ src/components/rich-actions-text-field/rich-actions-text-field.tsx
@@ -3,18 +3,6 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@radix-ui/react-popover"
-import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip"
-import { Button } from "@shadcn/components/ui/button"
-import { cn } from "@shadcn/lib/utils"
-import { Copy, Pencil } from "lucide-react"
-import copy from "copy-to-clipboard"
-import { useToast } from "@shadcn/hooks/use-toast"
 import { Actions } from "./actions"
 import { Content } from "./content"
 import { useState } from "react"
