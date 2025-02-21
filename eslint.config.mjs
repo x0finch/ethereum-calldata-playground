d7216c313e6b8f910b18e642cce1638dfc25ce79
x0finch <x0finch0x@gmail.com>
2025-02-22 01:19:07 +0800

    Disable ESLint rule for unescaped entities in React

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
       "react/display-name": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
